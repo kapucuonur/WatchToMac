71296c912edf917e52c213681d0ec4eb409c7912
kapucuonur <Gumushane1983.>
2026-03-01 13:40:00 +0000

    Add initial test targets for WatchToMac

---
+++ GarminToMac/WatchToMacTests/GarminToMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchToMacTests.swift
+//  WatchToMacTests
+//
+//  Created by Onur Kapucu on 27.12.2025.
+//
+
+import Testing
+@testable import WatchToMac
+
+struct WatchToMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
